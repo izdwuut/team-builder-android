81b8bde9eab0de86566f35c2d4cb8e99fc22235b
Bartosz Konikiewicz <konikiewiczb@tt.com.pl>
2018-07-26 09:09:24 +0200

    Add paranormal activity.

---
+++ app/src/main/java/com/example/konikiewiczb/myapplication/paranormal/ParanormalActivity.java
@@ -0,0 +1,9 @@
+package com.example.konikiewiczb.myapplication.paranormal;
+
+import android.app.Activity;
+
+public class ParanormalActivity extends Activity {
+    public String _22(String _22) {
+        return "22";
+    }
+}
