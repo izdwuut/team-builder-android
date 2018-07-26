65ddb325ed41955d7d29ce9f5d150d1ee27abee6
Bartosz Konikiewicz <konikiewiczb@tt.com.pl>
2018-07-26 10:03:16 +0200

    Add more fields to User entity.

--- app/src/main/java/com/example/konikiewiczb/myapplication/model/User.java
+++ app/src/main/java/com/example/konikiewiczb/myapplication/model/User.java
@@ -4,7 +4,10 @@ public class User {
     Integer id;
     String email;
     String password;
-
+    String name;
+    String surname;
+    String department;
+    String status;
     public User(String email, String password) {
         this.email = email;
         this.password = password;
@@ -25,5 +28,50 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
