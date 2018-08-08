43a095c3e25d8952c90147e5b04d4c007a93733e
Adrian Kowalski <adrian.kowalski@ttms.pl>
2018-08-08 11:09:28 +0200

    Drawer layout in user area with fragment's activity to replace

--- app/src/main/java/com/example/konikiewiczb/myapplication/projects/list/ProjectsListActivity.java
+++ app/src/main/java/com/example/konikiewiczb/myapplication/projects/list/ProjectsListActivity.java
@@ -3,6 +3,14 @@ package com.example.konikiewiczb.myapplication.projects.list;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -11,6 +19,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.konikiewiczb.myapplication.MessageFragment;
 import com.example.konikiewiczb.myapplication.R;
 import com.example.konikiewiczb.myapplication.login.LoginActivity;
 import com.example.konikiewiczb.myapplication.model.TokenRepository;
@@ -19,15 +28,28 @@ import com.example.konikiewiczb.myapplication.model.UserProject;
 import java.io.InputStream;
 import java.util.List;
 
-public class ProjectsListActivity extends Activity implements View.OnClickListener, ProjectsListContract.View {
+public class ProjectsListActivity extends AppCompatActivity implements View.OnClickListener, ProjectsListContract.View, NavigationView.OnNavigationItemSelectedListener{
     Button logout;
     ListView usersList;
     ProjectsListContract.Presenter presenter;
     ProjectsListContract.Interactor interactor;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projects_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
         logout = findViewById(R.id.logout);
         usersList = findViewById(R.id.users_list);
 
@@ -38,9 +60,29 @@ public class ProjectsListActivity extends Activity implements View.OnClickListen
     }
 
     @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public void onClick(View view) {
         presenter.logOut();
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                break;
+        }
+        return true;
     }
 
     @Override
