2d13489ff31653d7398c2ef9e457b3b16da61db1
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2018-08-16 15:23:36 +0200

    Register a LocalDateTime type adapter to the GsonConverter.

--- app/src/main/java/com/example/konikiewiczb/myapplication/framework/json/GsonConverter.java
+++ app/src/main/java/com/example/konikiewiczb/myapplication/framework/json/GsonConverter.java
@@ -2,6 +2,15 @@ package com.example.konikiewiczb.myapplication.framework.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class GsonConverter{
     static Gson gson;
@@ -11,7 +20,12 @@ public class GsonConverter{
 
     public static Gson get() {
         if(gson == null) {
-            gson = new GsonBuilder().create();
+            gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+                @Override
+                public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+                    return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString());
+                }
+            }).create();
         }
         return gson;
     }
