4668ef70c5aeb3b1d7d842caac3c7e81a105ffe0
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2018-08-21 11:39:59 +0200

    Make RetrofitClient use a Gson object provided by a GsonConverter.

--- app/src/main/java/com/example/konikiewiczb/myapplication/framework/json/GsonConverter.java
+++ app/src/main/java/com/example/konikiewiczb/myapplication/framework/json/GsonConverter.java
@@ -1,5 +1,7 @@
 package com.example.konikiewiczb.myapplication.framework.json;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
@@ -9,8 +11,12 @@ import com.google.gson.JsonParseException;
 
 import java.lang.reflect.Type;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class GsonConverter{
     static Gson gson;
@@ -23,7 +29,8 @@ public class GsonConverter{
             gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
                 @Override
                 public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-                    return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString());
+                    LocalDateTime dateTime = LocalDateTime.parse(json.getAsJsonPrimitive().getAsString());
+                    return dateTime;
                 }
             }).create();
         }
